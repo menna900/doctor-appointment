22a12a285b3edbe49eb03624646808274a25256e
menna900 <mennaosama837@gmail.com>
2025-09-09 13:57:35 +0300

    Update appointments.js

--- src/data/appointments.js
+++ src/data/appointments.js
@@ -1,12 +1,12 @@
 
 import { reactive, watch } from "vue";
 
-// نجيب البيانات من LocalStorage أو نعمل مصفوفة فاضية
+
 export const appointments = reactive(
   JSON.parse(localStorage.getItem("appointments") || "[]")
 );
 
-// نراقب أي تغيير يحصل في الـ appointments ونخزنه في LocalStorage
+
 watch(
   appointments,
   (newVal) => {
